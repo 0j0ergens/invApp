8521dc7081e1d5bedf41a7c7084e4e34b6c62418
0j0ergens <114615419+0j0ergens@users.noreply.github.com>
2024-09-16 10:49:41 -0400

    added my project

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const express = require('express');
+app = express();
+ 
+app.get('/', function (req, res) {
+    res.send('Hello world'); 
+});
+ 
+app.listen(4000, function () {
+    console.log('http://localhost:4000/');
+});
+
