3dce4e971aec9f66d860b0cabf62b00052582c30
0j0ergens <114615419+0j0ergens@users.noreply.github.com>
2024-09-17 13:49:20 -0400

    first

--- invApp/App.js
+++ invApp/App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, ImageBackground, StyleSheet, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+
+// Use require for local assets
+const image = require('./assets/fruit2.png');
+
+const App = () => (
+/*
+  const onPressLogin= () => {
+    const [titleText, setTitleText] = useState("Bird's Nest");
+
+    setTitleText("Bird's Nest [pressed]");
+  };
+*/
+
+  <View style={styles.container}>
+    <ImageBackground
+      source={image} 
+      resizeMode="cover"
+      style={styles.image}
+    >
+      <Text style = {styles.title}> Inventory. </Text>
+      <View style = {styles.underContainer}> 
+        <Text style = {styles.under}> Login </Text>
+        <Text style = {styles.under}> SignUp </Text>
+      </View>
+     
+    </ImageBackground>
+  </View>
+);
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  title: {
+    fontSize: 90, 
+    color: 'white', 
+    top: '10%',
+    fontFamily: 'Farah'
+  },
+
+  underContainer: {
+    flexDirection: 'row', 
+    top: 80,
+  }, 
+
+  under: {
+    color: 'red', 
+    fontSize: 40, 
+    
+    marginHorizontal: 30, 
+    left: -27, 
+    fontFamily: 'Farah'
+  }, 
+
+  image: {
+    flex: 1, 
+    justifyContent: 'left', 
+    
+  }
 });
+
+export default App;
